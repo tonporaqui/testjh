8285d1e344d6aa48c36dd990ce6d5a88a404ddbc
Gaston <cutkillerone@gmail.com>
2023-11-21 17:11:18 -0300

    aplicando cors y actualizando json thunder para el crud usado

--- src/main/java/com/jh/test/config/SecurityConfiguration.java
+++ src/main/java/com/jh/test/config/SecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.jh.test.config;
 
-import com.jh.test.security.AuthoritiesConstants;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +12,9 @@ import org.springframework.security.oauth2.server.resource.web.access.BearerToke
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import com.jh.test.security.AuthoritiesConstants;
+
 import tech.jhipster.config.JHipsterProperties;
 
 @Configuration
@@ -28,8 +30,9 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, MvcRequestMatcher.Builder mvc) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(authz ->
+                .cors(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(authz ->
                 // prettier-ignore
                 authz
                         .requestMatchers(mvc.pattern(HttpMethod.POST, "/api/authenticate")).permitAll()
@@ -47,15 +50,12 @@ public class SecurityConfiguration {
                         .requestMatchers(mvc.pattern("/management/health/**")).permitAll()
                         .requestMatchers(mvc.pattern("/management/info")).permitAll()
                         .requestMatchers(mvc.pattern("/management/prometheus")).permitAll()
-                        .requestMatchers(mvc.pattern("/management/**")).hasAuthority(AuthoritiesConstants.ADMIN)
-            )
-            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-            .exceptionHandling(exceptions ->
-                exceptions
-                    .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
-                    .accessDeniedHandler(new BearerTokenAccessDeniedHandler())
-            )
-            .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+                        .requestMatchers(mvc.pattern("/management/**")).hasAuthority(AuthoritiesConstants.ADMIN))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptions -> exceptions
+                        .authenticationEntryPoint(new BearerTokenAuthenticationEntryPoint())
+                        .accessDeniedHandler(new BearerTokenAccessDeniedHandler()))
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
         return http.build();
     }
 
