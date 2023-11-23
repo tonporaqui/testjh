39346da259fa6c35f5b19e8aa85431d85ea27e9c
Gaston <cutkillerone@gmail.com>
2023-11-23 18:21:16 -0300

    Termiando de incorporar Excel y Word a lo inicial del PDF de jasper report.

--- src/main/java/com/jh/test/service/ReportService.java
+++ src/main/java/com/jh/test/service/ReportService.java
@@ -1,5 +1,6 @@
 package com.jh.test.service;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.sql.Connection;
@@ -17,9 +18,17 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.export.ooxml.JRDocxExporter;
+import net.sf.jasperreports.engine.export.ooxml.JRXlsxExporter;
+import net.sf.jasperreports.export.SimpleDocxReportConfiguration;
+import net.sf.jasperreports.export.SimpleExporterInput;
+import net.sf.jasperreports.export.SimpleOutputStreamExporterOutput;
+import net.sf.jasperreports.export.SimpleXlsxReportConfiguration;
 
 /**
  * Service class for managing reports.
+ * This service provides functionality to generate reports in PDF, Excel, and
+ * Word formats.
  */
 @Service
 public class ReportService {
@@ -28,6 +37,7 @@ public class ReportService {
 
     /**
      * Constructor for ReportService.
+     * Initializes the service with the provided DataSource.
      *
      * @param dataSource the data source for database connection.
      */
@@ -37,27 +47,82 @@ public class ReportService {
 
     /**
      * Generate a report in PDF format and encode it in Base64.
+     * This method compiles a Jasper report template and fills it with data from the
+     * database.
      *
      * @return a Base64 encoded string of the generated PDF report.
      * @throws Exception if there is an error during report generation.
      */
-    public String generateReport() throws Exception {
-        // Locate the .jrxml file
+    public String generatePdfReport() throws Exception {
         File file = ResourceUtils.getFile("classpath:reports/list_user.jrxml");
-
-        // Compile the report
         JasperReport jasperReport = JasperCompileManager.compileReport(new FileInputStream(file));
-
-        // Parameters for the report (if needed)
         Map<String, Object> parameters = new HashMap<>();
 
-        // Connect to the database and fill the report
+        try (Connection connection = dataSource.getConnection()) {
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+            byte[] pdfContent = JasperExportManager.exportReportToPdf(jasperPrint);
+            return Base64.getEncoder().encodeToString(pdfContent);
+        }
+    }
+
+    /**
+     * Generate a report in Excel format and encode it in Base64.
+     * This method compiles a Jasper report template, fills it with data, and
+     * exports it to Excel format.
+     *
+     * @return a Base64 encoded string of the generated Excel report.
+     * @throws Exception if there is an error during report generation.
+     */
+    public String generateExcelReport() throws Exception {
+        File file = ResourceUtils.getFile("classpath:reports/list_user.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(new FileInputStream(file));
+        Map<String, Object> parameters = new HashMap<>();
+
         try (Connection connection = dataSource.getConnection()) {
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
 
-            // Export to PDF and encode in Base64
-            byte[] pdfContent = JasperExportManager.exportReportToPdf(jasperPrint);
-            return Base64.getEncoder().encodeToString(pdfContent);
+            JRXlsxExporter exporter = new JRXlsxExporter();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(os));
+
+            SimpleXlsxReportConfiguration configuration = new SimpleXlsxReportConfiguration();
+            configuration.setOnePagePerSheet(true);
+            exporter.setConfiguration(configuration);
+
+            exporter.exportReport();
+            return Base64.getEncoder().encodeToString(os.toByteArray());
+        }
+    }
+
+    /**
+     * Generate a report in Word format (DOCX) and encode it in Base64.
+     * This method compiles a Jasper report template, fills it with data, and
+     * exports it to Word format.
+     *
+     * @return a Base64 encoded string of the generated Word report.
+     * @throws Exception if there is an error during report generation.
+     */
+    public String generateWordReport() throws Exception {
+        File file = ResourceUtils.getFile("classpath:reports/list_user.jrxml");
+        JasperReport jasperReport = JasperCompileManager.compileReport(new FileInputStream(file));
+        Map<String, Object> parameters = new HashMap<>();
+
+        try (Connection connection = dataSource.getConnection()) {
+            JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters, connection);
+
+            JRDocxExporter exporter = new JRDocxExporter();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            exporter.setExporterInput(new SimpleExporterInput(jasperPrint));
+            exporter.setExporterOutput(new SimpleOutputStreamExporterOutput(os));
+
+            SimpleDocxReportConfiguration configuration = new SimpleDocxReportConfiguration();
+            exporter.setConfiguration(configuration);
+
+            exporter.exportReport();
+            return Base64.getEncoder().encodeToString(os.toByteArray());
         }
     }
 }
--- src/main/java/com/jh/test/web/rest/ReportResource.java
+++ src/main/java/com/jh/test/web/rest/ReportResource.java
@@ -14,6 +14,8 @@ import tech.jhipster.web.util.HeaderUtil;
 
 /**
  * REST controller for managing reports.
+ * This controller provides endpoints to generate and retrieve reports in
+ * different formats like PDF, Excel, and Word.
  */
 @RestController
 @RequestMapping("/api/report")
@@ -33,27 +35,87 @@ public class ReportResource {
     }
 
     /**
-     * {@code GET  /report} : Generate and get a report.
+     * {@code GET  /report/pdf} : Generate and get a report in PDF format.
+     * This endpoint generates a report based on data and returns it in PDF format
+     * encoded in Base64.
      *
      * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with the
-     *         report data,
+     *         PDF report data,
      *         or with status {@code 500 (Internal Server Error)} if there was an
-     *         error generating the report.
+     *         error generating the PDF report.
      */
-    @GetMapping("")
-    public ResponseEntity<String> getReport() {
-        log.debug("REST request to get a report");
+    @GetMapping("/pdf")
+    public ResponseEntity<String> getPdfReport() {
+        log.debug("REST request to get a PDF report");
         try {
-            String report = reportService.generateReport();
+            String report = reportService.generatePdfReport();
             return ResponseEntity.ok()
                     .headers(
-                            HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME, "reportGenerated"))
+                            HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME,
+                                    "pdfReportGenerated"))
                     .body(report);
         } catch (Exception e) {
-            log.error("REST request to get a report failed: {}", e.getMessage());
+            log.error("REST request to get a PDF report failed: {}", e.getMessage());
             return ResponseEntity.internalServerError()
-                    .headers(HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "reportNotGenerated",
-                            "There was an error generating the report"))
+                    .headers(HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "pdfReportNotGenerated",
+                            "There was an error generating the PDF report"))
+                    .build();
+        }
+    }
+
+    /**
+     * {@code GET  /report/excel} : Generate and get a report in Excel format.
+     * This endpoint generates a report based on data and returns it in Excel format
+     * encoded in Base64.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with the
+     *         Excel report data,
+     *         or with status {@code 500 (Internal Server Error)} if there was an
+     *         error generating the Excel report.
+     */
+    @GetMapping("/excel")
+    public ResponseEntity<String> getExcelReport() {
+        log.debug("REST request to get an Excel report");
+        try {
+            String report = reportService.generateExcelReport();
+            return ResponseEntity.ok()
+                    .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME,
+                            "excelReportGenerated"))
+                    .body(report);
+        } catch (Exception e) {
+            log.error("REST request to get an Excel report failed: {}", e.getMessage());
+            return ResponseEntity.internalServerError()
+                    .headers(
+                            HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "excelReportNotGenerated",
+                                    "There was an error generating the Excel report"))
+                    .build();
+        }
+    }
+
+    /**
+     * {@code GET  /report/word} : Generate and get a report in Word format.
+     * This endpoint generates a report based on data and returns it in Word format
+     * encoded in Base64.
+     *
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with the
+     *         Word report data,
+     *         or with status {@code 500 (Internal Server Error)} if there was an
+     *         error generating the Word report.
+     */
+    @GetMapping("/word")
+    public ResponseEntity<String> getWordReport() {
+        log.debug("REST request to get a Word report");
+        try {
+            String report = reportService.generateWordReport();
+            return ResponseEntity.ok()
+                    .headers(HeaderUtil.createEntityCreationAlert(applicationName, true, ENTITY_NAME,
+                            "wordReportGenerated"))
+                    .body(report);
+        } catch (Exception e) {
+            log.error("REST request to get a Word report failed: {}", e.getMessage());
+            return ResponseEntity.internalServerError()
+                    .headers(HeaderUtil.createFailureAlert(applicationName, true, ENTITY_NAME, "wordReportNotGenerated",
+                            "There was an error generating the Word report"))
                     .build();
         }
     }
