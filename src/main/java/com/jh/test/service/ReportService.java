f347120ba1d396430916c388af5aac47a2e088ce
Gaston <cutkillerone@gmail.com>
2023-11-29 11:48:51 -0300

    encriptando pdf con contraseña.

--- src/main/java/com/jh/test/service/ReportService.java
+++ src/main/java/com/jh/test/service/ReportService.java
@@ -1,5 +1,6 @@
 package com.jh.test.service;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -16,6 +17,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
+import com.itextpdf.kernel.pdf.EncryptionConstants;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfReader;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.kernel.pdf.WriterProperties;
 import com.jh.test.service.dto.AppUserDTO;
 
 import net.sf.jasperreports.engine.JasperCompileManager;
@@ -61,6 +67,22 @@ public class ReportService {
      * @return a Base64 encoded string of the generated PDF report.
      * @throws Exception if there is an error during report generation.
      */
+    // public String generatePdfReport() throws Exception {
+    // Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE);
+    // List<AppUserDTO> appUserDTOs = appUserService.findAll(pageable).getContent();
+    // File file = ResourceUtils.getFile("classpath:reports/list_user.jrxml");
+    // JasperReport jasperReport = JasperCompileManager.compileReport(new
+    // FileInputStream(file));
+    // Map<String, Object> parameters = new HashMap<>();
+
+    // try (Connection connection = dataSource.getConnection()) {
+    // JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport,
+    // parameters,
+    // new JRBeanCollectionDataSource(appUserDTOs));
+    // byte[] pdfContent = JasperExportManager.exportReportToPdf(jasperPrint);
+    // return Base64.getEncoder().encodeToString(pdfContent);
+    // }
+    // }
     public String generatePdfReport() throws Exception {
         Pageable pageable = PageRequest.of(0, Integer.MAX_VALUE);
         List<AppUserDTO> appUserDTOs = appUserService.findAll(pageable).getContent();
@@ -71,8 +93,27 @@ public class ReportService {
         try (Connection connection = dataSource.getConnection()) {
             JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, parameters,
                     new JRBeanCollectionDataSource(appUserDTOs));
-            byte[] pdfContent = JasperExportManager.exportReportToPdf(jasperPrint);
-            return Base64.getEncoder().encodeToString(pdfContent);
+
+            ByteArrayOutputStream pdfOutputStream = new ByteArrayOutputStream();
+            ByteArrayOutputStream encryptedPdfOutputStream = new ByteArrayOutputStream();
+
+            // Exporta a un OutputStream en lugar de un archivo directamente
+            JasperExportManager.exportReportToPdfStream(jasperPrint, pdfOutputStream);
+
+            // Configuración de la contraseña y permisos para el PDF
+            WriterProperties writerProperties = new WriterProperties()
+                    .setStandardEncryption(
+                            "1234".getBytes(), // Contraseña del usuario
+                            "ownerPassword".getBytes(), // Contraseña del propietario, editar y otros...
+                            EncryptionConstants.ALLOW_PRINTING,
+                            EncryptionConstants.ENCRYPTION_AES_128 | EncryptionConstants.DO_NOT_ENCRYPT_METADATA);
+
+            PdfWriter pdfWriter = new PdfWriter(encryptedPdfOutputStream, writerProperties);
+            PdfDocument pdfDocument = new PdfDocument(
+                    new PdfReader(new ByteArrayInputStream(pdfOutputStream.toByteArray())), pdfWriter);
+            pdfDocument.close();
+
+            return Base64.getEncoder().encodeToString(encryptedPdfOutputStream.toByteArray());
         }
     }
 
